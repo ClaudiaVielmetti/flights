2ffb5f7e7e6978b7e3f7315387d59341ea38bf44
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-10-31 11:39:43 +0200

    Implement client API to show flight data

--- Flights/Flights/ClientApp/src/app/search-flights/search-flights.component.ts
+++ Flights/Flights/ClientApp/src/app/search-flights/search-flights.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Time } from '@angular/common';
+import { FlightService } from './../api/services/flight.service';
+import { FlightRm } from './../api/models';
 
 @Component({
   selector: 'app-search-flights',
@@ -8,45 +10,17 @@ import { Time } from '@angular/common';
 })
 export class SearchFlightsComponent implements OnInit {
 
-  searchResult: FlightRm[] = [
-    {
-      airline: "American Airlines",
-      remainingNumberOfSeats: 500,
-      departure: { time: Date.now().toString(), place: "Los Angeles" },
-      arrival: { time: Date.now().toString(), place: "Istanbul" },
-      price: "350",
-    },
-    {
-      airline: "Deutsche BA",
-      remainingNumberOfSeats: 60,
-      departure: { time: Date.now().toString(), place: "Munchen" },
-      arrival: { time: Date.now().toString(), place: "Schiphol" },
-      price: "600",
-    },
-    {
-      airline: "British Airways",
-      remainingNumberOfSeats: 60,
-      departure: { time: Date.now().toString(), place: "London, England" },
-      arrival: { time: Date.now().toString(), place: "Vizzola-Ticino" },
-      price: "600",
-    },
-  ]
+  searchResult: FlightRm[] = []
 
-  constructor() { }
+
+  constructor(private flightService: FlightService) { }
 
   ngOnInit(): void { }
 
+  search() {
+    this.flightService.flightGet({})
+    .subscribe(response => this.searchResult = response)
+  }
 }
 
-export interface FlightRm {
-  airline: string;
-  arrival: TimePlaceRm;
-  departure: TimePlaceRm;
-  price: string;
-  remainingNumberOfSeats: number;
-}
 
-export interface TimePlaceRm {
-  place: string;
-  time: string;
-}
