6ae52716df5d6a816fb1f41d30240df4922fbf22
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-10-31 10:32:21 +0200

    adding swagger

--- Flights/Flights/Program.cs
+++ Flights/Flights/Program.cs
@@ -1,10 +1,20 @@
+using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddServer(new OpenApiServer
+    {
+        Description = "Development Server",
+        Url = "https://localhost:7160"
+    });
+});
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
