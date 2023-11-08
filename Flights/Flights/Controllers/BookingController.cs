4f76525dc0132fdb0cd003bfe8ee0b2a40cb9da7
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-11-08 12:18:51 +0200

    Layout My-Bookings

---
+++ Flights/Flights/Controllers/BookingController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Flights.Data;
+using Flights.ReadModels;
+
+
+namespace Flights.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly Entities _entities;
+
+        public BookingController(Entities entities)
+        {
+            _entities = entities;
+        }
+
+        [HttpGet("{email}")]
+        [ProducesResponseType(typeof(IEnumerable<BookingRm>),200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+       public ActionResult<IEnumerable<BookingRm>> List(string email)
+        {
+
+        }
+    }
+}
