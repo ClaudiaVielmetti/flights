b9402e6f9492048e6d9544f98db258870dc57e70
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-11-08 12:38:48 +0200

    Get Booking List Endpoint

--- Flights/Flights/Controllers/BookingController.cs
+++ Flights/Flights/Controllers/BookingController.cs
@@ -21,9 +21,23 @@ namespace Flights.Controllers
         [ProducesResponseType(typeof(IEnumerable<BookingRm>),200)]
         [ProducesResponseType(400)]
         [ProducesResponseType(500)]
+
        public ActionResult<IEnumerable<BookingRm>> List(string email)
         {
+            var bookings = _entities.Flights.ToArray()
+                .SelectMany(f => f.Bookings
+                    .Where(b => b.PassengerEmail == email)
+                    .Select(b => new BookingRm(
+                        f.Id,
+                        f.Airline,
+                        f.Price.ToString(),
+                        new TimePlaceRm(f.Arrival.Place, f.Arrival.Time),
+                        new TimePlaceRm(f.Departure.Place, f.Departure.Time),
+                        b.NumberOfSeats,
+                        email
+                        )));
 
+            return Ok(bookings);
         }
     }
 }
