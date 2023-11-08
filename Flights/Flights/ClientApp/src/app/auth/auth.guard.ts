28dc2536d894a339aa3e306f269d36423ebce7a8
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-11-08 14:25:52 +0200

    Implement Route Guards

---
+++ Flights/Flights/ClientApp/src/app/auth/auth.guard.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService,
+    private router: Router) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    if (!this.authService.currentUser)
+      this.router.navigate(['/register-passenger'])
+
+    return true;
+  }
+  
+}
+
+
+// If CanActivate Depriciates to non working condition her is back up code
+
+
+//import { CanActivateFn, Router } from '@angular/router';
+//import { AuthService } from '../services/auth.service';
+//import { inject } from '@angular/core';
+
+//export const authGuard: CanActivateFn = (route, state) => {
+
+//  const currentUser = inject(AuthService).currentUser;
+
+//  if (!currentUser)
+//    inject(Router).navigate(['/register-passenger']);
+
+//  return true;
+//};
